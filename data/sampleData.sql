5d5a7d5b9c6063079fdb27c701ce8ee033edb51a
macintro1 <marcos.cintron@fixadera.com>
2024-06-28 10:07:38 -0700

    Just some sample data.

---
+++ data/sampleData.sql
@@ -0,0 +1,218 @@
+INSERT INTO `nrdemo`.`companyinfo`
+(`companyName`,
+`contactEmail`)
+VALUES
+('Acme Inc.',
+'support@acmeinc.com');
+
+INSERT INTO `nrdemo`.`companyinfo`
+(`companyName`,
+`contactEmail`)
+VALUES
+('Vans Aircraft',
+'support@vansaircraft.com');
+
+INSERT INTO `nrdemo`.`companyinfo`
+(`companyName`,
+`contactEmail`)
+VALUES
+('Nike',
+'support@nike.com');
+
+INSERT INTO `nrdemo`.`companyinfo`
+(`companyName`,
+`contactEmail`)
+VALUES
+('Columbia',
+'support@columbia.com');
+
+
+
+
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Josh',
+'Johnson',
+'jj@acme.com',
+'26493628-34a4-11ef-b59b-5405dba18573');
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Kurt',
+'Russell',
+'krl@acme.com',
+'26493628-34a4-11ef-b59b-5405dba18573');
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Dan',
+'Moffat',
+'dm@acme.com',
+'26493628-34a4-11ef-b59b-5405dba18573');
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Roberto',
+'Huerta',
+'krl@acme.com',
+'26493628-34a4-11ef-b59b-5405dba18573');
+
+
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('James',
+'Hansen',
+'james@vansaircraft.com',
+'18ef2746-34a5-11ef-b59b-5405dba18573');
+
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Ross',
+'Rodgers',
+'ross@vansaircraft.com',
+'18ef2746-34a5-11ef-b59b-5405dba18573');
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('James',
+'Hansen',
+'james@vansaircraft.com',
+'18ef2746-34a5-11ef-b59b-5405dba18573');
+
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Craig',
+'Rollings',
+'craig@vansaircraft.com',
+'18ef2746-34a5-11ef-b59b-5405dba18573');
+
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Rod',
+'Machado',
+'rod@vansaircraft.com',
+'18ef2746-34a5-11ef-b59b-5405dba18573');
+
+
+
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Jose',
+'Rodriguez',
+'jRodriguez@vansaircraft.com',
+'18f058bd-34a5-11ef-b59b-5405dba18573');
+
+
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Tara',
+'Churchill',
+'tChurchill@nike.com',
+'18f058bd-34a5-11ef-b59b-5405dba18573');
+
+
+
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Corey',
+'Wilkinson',
+'cWilkinson@nike.com',
+'18f058bd-34a5-11ef-b59b-5405dba18573');
+
+
+
+
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Stephanie',
+'Mfoshy',
+'sm@columbia.com',
+'18f0e166-34a5-11ef-b59b-5405dba18573');
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Kelly',
+'Dorious',
+'kd@columbia.com',
+'18f0e166-34a5-11ef-b59b-5405dba18573');
+
+
+INSERT INTO `nrdemo`.`customerinfo`
+(`fName`,
+`lName`,
+`email`,
+`companyID`)
+VALUES
+('Taja',
+'Marie',
+'tm@columbia.com',
+'18f0e166-34a5-11ef-b59b-5405dba18573');
+
+
+
+
